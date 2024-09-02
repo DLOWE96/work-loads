078e2217f59fdf36237c5c54230d30328e1387ab
DLOWE96 <dannylowe96@hotmail.co.uk>
2024-09-02 21:34:00 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#ask user to input their name
+#store input as a variable called "name"
+#print out the users name on the following line
+#ask user to input their age
+#store input as a variable called "age"
+#print out the users age on the following line
+#on the last line print "Hello World!"
+
+name = input("What is your name ")
+print(name)
+
+age = input("What is your age ")
+print(age)
+
+print("Hello World"+"!")
