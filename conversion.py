213b67a711d2a141cf8484c41625dc9bf338d093
DLOWE96 <dannylowe96@hotmail.co.uk>
2024-09-02 21:57:17 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,26 @@
+#create variables for "num1", "num2", "num3", & "string1"
+#declare these variables
+
+#convert "num1" into an integer using int()
+#convert "num 2" into a float using float()
+#convert "num 3" into a string using str()
+#convert "string1" into an integer using int()
+
+#print all variables on seperate lines following conversion
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+int(num1)
+float(num2)
+str(num3)
+int(string1)
+
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
+
+
